33469f492c73b54007f27e6c9b0a11edb5d79fb4
PSILVA999 <p.silva0399@gmail.com>
2022-12-29 06:25:33 -0300

    config do github-pages

--- src/Components/Galio.jsx
+++ src/Components/Galio.jsx
@@ -11,12 +11,15 @@ import Grid from '../assets/imgs/4-galio/6.png'
 import Compress from '../assets/imgs/4-galio/7.png'
 import Path from '../assets/imgs/4-galio/8.png'
 import Shadow from '../assets/imgs/4-galio/9.png'
-
 import Imgbb from '../assets/imgs/4-galio/10.png'
-import Uns from '../assets/imgs/4-galio/11.png'
 
+import Uns from '../assets/imgs/4-galio/11.png'
 import Uiverse from '../assets/imgs/4-galio/12.jpg'
 import Coolors from '../assets/imgs/4-galio/13.jpg'
+
+import Remix from '../assets/imgs/4-galio/14.jpg'
+import Icons8 from '../assets/imgs/4-galio/15.png'
+import Oneapp from '../assets/imgs/4-galio/16.png'
 
 const Galio = () => {
   return (
@@ -84,6 +87,21 @@ const Galio = () => {
       <a href="https://coolors.co/eabfcb-c191a1-a4508b-5f0a87-2f004f" target='_blank'>
         <img src={ Coolors }/>
         <span>Coolors</span> 
+      </a>
+
+      <a href="https://remixicon.com/" target='_blank'>
+        <img src={ Remix }/>
+        <span>Remixicon</span> 
+      </a>
+
+      <a href="https://icons8.com.br/" target='_blank'>
+        <img src={ Icons8 }/>
+        <span>Icons8</span> 
+      </a>
+
+      <a href="https://123apps.com/pt/" target='_blank'>
+        <img src={ Oneapp }/>
+        <span>123apps</span> 
       </a>
     </main>
   )
